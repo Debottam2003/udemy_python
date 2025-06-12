df31d57240a206315ae21b9f78facec082113518
Debottam Kar <debottamkar2003@gmail.com>
2025-06-12 21:32:35 +0530

    decorator function

--- 05_Function.py
+++ 05_Function.py
@@ -1,9 +1,22 @@
-from module1.m1py import do
+# from module1.m1py import do
+from m1py import do
 print("Let's learn all about function")
 name = "debottam"
 def printName(name):
     print(name)
 
+# decorator function
+def my_decorator(function):
+    req = {"body": "this is the request body"}
+    def say_hello():
+        print(req)
+        function()
+    return say_hello
+
 if __name__ == "__main__":
     printName(name)
     do()
+    @my_decorator #say_hello = my_decorator(say_hello)
+    def say_hello():
+        print("hello from debottam.")
+    say_hello()
