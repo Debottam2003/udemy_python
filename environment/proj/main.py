67c788d13df4e120d485bd8d1f853e59183b7350
Debottam Kar <debottamkar2003@gmail.com>
2025-08-14 21:45:46 +0530

    backend frameworks

--- environment/proj/main.py
+++ environment/proj/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from proj!")
+# from flask import Flask
 
+# app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+# @app.get("/")
+# def home():
+#     return "hello world"
+
+# @app.post("/")
+# def save():
+#     return "data saved"
+
+# if __name__ == "__main__":
+#     print("Hello from proj!")
+#     app.run(host="127.0.0.1", port=3333, debug=True)
+
+from fastapi import FastAPI
+from uvicorn import run
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"data": {"name": "debottam kar", "age": 22}}
+
+run(app, host="127.0.0.1", port=3333)
