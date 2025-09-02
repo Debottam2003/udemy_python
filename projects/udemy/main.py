e8a1130bd24fd51bc8bcbdb4fec0cab2c63d634d
Debottam Kar <debottamkar2003@gmail.com>
2025-09-02 16:32:35 +0530

    two problems done from udemy course

---
+++ projects/udemy/main.py
@@ -0,0 +1,23 @@
+def main():
+    print("Hello from udemy!")
+    # def calculate_love_score(name1, name2):
+    #     true = []
+    #     love = []
+    #     str1 = "true"
+    #     str2 = "love"
+    #     for i in str1:
+    #         u = name1.count(i)
+    #         v = name2.count(i)
+    #         true.append(u + v)
+    #     for i in str2:
+    #         u = name1.count(i)
+    #         v = name2.count(i)
+    #         love.append(u + v)
+    #     print(true)
+    #     print(love)
+    #     print(sum(true) * 10 + sum(love))
+    # calculate_love_score("Kanye West", "Kim Kardashian")
+            
+if __name__ == "__main__":
+    main()
+
